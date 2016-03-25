438685b7ef41777a982bce715ac072f503b011a6
MEWsofCastemere <malachiw@utexas.edu>
2016-03-24 20:53:10 -0500

    no message

---
+++ app/src/main/java/com/example/tan/colormatch/Level.java
@@ -0,0 +1,13 @@
+package com.example.tan.colormatch;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Level extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level);
+    }
+}
